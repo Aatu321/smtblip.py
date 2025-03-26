9d36b2e576a9c6884f85beb70d2b40df85f9c587
yashmini_21 <miholiayashmini@gmail.com>
2025-03-26 02:29:53 -0500

    Create README.py

---
+++ README.py
@@ -0,0 +1,13 @@
+import smtplib
+
+
+sender_mail = "miholiayashmini@gmail.com"
+sender_passwd = "jzwuowbqqdwxrfnk "
+recevier_mail = "ymiholia@gmail.com"
+subject = "hello paji how are you"
+
+s = smtplib.SMTP("smtp.gmail.com",587)
+s.starttls()
+s.login(sender_mail,sender_passwd)
+s.sendmail(sender_mail,recevier_mail,subject)
+s.quit()
